6455f3686e00fba6c42f0cadb04d500e6fc07c66
chendx97 <3310115926@qq.com>
2025-02-10 23:16:45 +0800

    config: 修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:3000',
-        target: 'http://fyzzz.cn:9100',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       }
     }
