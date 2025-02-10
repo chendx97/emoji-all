377dcd4d78d061d17634eead49e5e6fa10668945
chendx97 <3310115926@qq.com>
2025-02-10 23:27:14 +0800

    config: 修改配置

--- src/utils/myFetch.ts
+++ src/utils/myFetch.ts
@@ -1,7 +1,8 @@
+const baseUrl = import.meta.env.PROD ? 'http://fyzzz.cn:9100' : '';
 export const getReq = async (url: string, params?: { [key: string]: any }) => {
   const queryString = params ? new URLSearchParams(params).toString() : '';
   const fullUrl = queryString ? `${url}?${queryString}` : url;
-  const res = await fetch(fullUrl, { method: 'GET' });
+  const res = await fetch(`${baseUrl}${fullUrl}`, { method: 'GET' });
   const data = await res.json();
   return data;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://fyzzz.cn:9100',
         changeOrigin: true,
       }
     }
