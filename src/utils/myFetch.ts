a98e5c483f56cd3feccc1883adb7bbb30ef5f037
chendx97 <3310115926@qq.com>
2025-02-12 20:58:01 +0800

    config: 添加token

--- src/utils/myFetch.ts
+++ src/utils/myFetch.ts
@@ -1,8 +1,14 @@
-const baseUrl = import.meta.env.PROD ? '//fyzzz.cn' : '';
+const baseUrl = import.meta.env.PROD ? 'https://fyzzz.cn' : '';
 export const getReq = async (url: string, params?: { [key: string]: any }) => {
   const queryString = params ? new URLSearchParams(params).toString() : '';
   const fullUrl = queryString ? `${url}?${queryString}` : url;
-  const res = await fetch(`${baseUrl}${fullUrl}`, { method: 'GET' });
+  const res = await fetch(`${baseUrl}${fullUrl}`,
+    {
+      method: 'GET',
+      headers: {
+        'token': import.meta.env.VITE_API_TOKEN || '',
+      },
+    });
   const data = await res.json();
   return data;
 };
